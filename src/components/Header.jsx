0a24b3e52cf5e7e9665dca64f01534e84d61b7cc
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-05-14 21:06:07 +0100

    Modified style to responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,33 +22,38 @@ export const Header = () => {
   }, [darkMode]);
 
   return (
-    <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6 mb-[980px]">
-      <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-1 ">
-        Social Media Dashboard
-      </h1>
-      <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-6">
-        Total Followers: 23,004
-      </p>
-      <hr className="bg-black h-[2px] mb-[19px]" />
-      <div className="flex justify-between">
-        <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold">
-          {" "}
-          Dark Mode
-        </p>
-        <label
-          htmlFor="darkmode"
-          className="border bg-Toggle w-12 h-6 rounded-full cursor-pointer p-[2px] relative overflow-hidden"
-        >
-          {/* Using "peer" when the input (hidden by sr-only) is clicked, change the div peer. peer just can be use in sibling elements */}
-          <input
-            onClick={handleClick}
-            type="checkbox"
-            id="darkmode"
-            className="peer sr-only"
-          />
-          <div className="w-full h-full absolute top-0  left-0 peer-checked:bg-Toggle-Gradient "></div>
-          <div className="w-[18px] h-[18px] bg-Light-Grayish-Blue rounded-full peer-checked:translate-x-[24px] transition-all z-10"></div>
-        </label>
+    <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6 mb-[1050px] md:mb-[480px] xl:mb-[200px] max-w-[1440px] mx-auto">
+      <div className="md:flex md:justify-between max-w-[1140] mx-auto">
+        <div>
+          <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-1 ">
+            Social Media Dashboard
+          </h1>
+          <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-6">
+            Total Followers: 23,004
+          </p>
+          <hr className="bg-black h-[2px] mb-[19px] md:hidden" />
+        </div>
+
+        <div className="flex justify-between gap-[13px]">
+          <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold">
+            {" "}
+            Dark Mode
+          </p>
+          <label
+            htmlFor="darkmode"
+            className="border bg-Toggle w-12 h-6 rounded-full cursor-pointer p-[2px] relative overflow-hidden"
+          >
+            {/* Using "peer" when the input (hidden by sr-only) is clicked, change the div peer. peer just can be use in sibling elements */}
+            <input
+              onClick={handleClick}
+              type="checkbox"
+              id="darkmode"
+              className="peer sr-only"
+            />
+            <div className="w-full h-full absolute top-0  left-0 peer-checked:bg-Toggle-Gradient "></div>
+            <div className="w-[18px] h-[18px] bg-Light-Grayish-Blue rounded-full peer-checked:translate-x-[24px] transition-all z-10"></div>
+          </label>
+        </div>
       </div>
     </header>
   );
--- src/components/OverviewContainer.jsx
+++ src/components/OverviewContainer.jsx
@@ -12,7 +12,7 @@ const convertNumberToK = (number) => {
 // console.log(data.overview);
 export const OverviewContainer = () => {
   return (
-    <section className="absolute top-[191px] left-0 right-0">
+    <section className="max-w-[1440px] flex flex-wrap gap-[30px] place-content-center absolute top-[191px] left-0 right-0 mx-auto xl:px-4 ">
       {data.overview.map((object) => (
         <OverviewCard
           key={object.id}
@@ -31,18 +31,22 @@ export const OverviewContainer = () => {
 // console.log(data["overview-today"]);
 export const OverviewTodayContainer = () => {
   return (
-    <section>
-      <h1>Overview - Today</h1>
-      {data["overview-today"].map((object) => (
-        <OverviewTodayCard
-          key={object.id}
-          network={object.network}
-          statsType={object.statsType}
-          stats={convertNumberToK(object.stats)}
-          percentage={object.percentage}
-          isUp={object.isUp}
-        />
-      ))}
+    <section className="max-w-[1440px] mx-auto">
+      <h2 className="text-2xl font-bold mb-[27px] text-Dark-Grayish-Blue ml-8 dark:text-white">
+        Overview - Today
+      </h2>
+      <div className="flex flex flex-wrap">
+        {data["overview-today"].map((object) => (
+          <OverviewTodayCard
+            key={object.id}
+            network={object.network}
+            statsType={object.statsType}
+            stats={convertNumberToK(object.stats)}
+            percentage={object.percentage}
+            isUp={object.isUp}
+          />
+        ))}
+      </div>
     </section>
   );
 };
